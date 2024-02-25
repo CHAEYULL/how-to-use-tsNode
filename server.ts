4f5e07ea626bd52ab4b392b422cf9853d0b4047d
leo <leopportunity@gmail.com>
2024-02-26 01:13:53 +0900

    jest 설치 완

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@
 /*
   "strictNullChecks": true,
  */
+//테스트 코드 작성하려면 npm install jest --save-dev
 //nodemon --exec ts-node server.ts
 import 'dotenv/config'
 //몽고디비 세팅
@@ -16,18 +17,20 @@ app.set('view engine', 'ejs')
 //요청.body 쓰려면 필요한 세팅 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+//여기부터
 let db : Db
 let changeStream: any;
 const url = "mongodb+srv://CHAEYULL:Firstrjtm1!@cluster0.kerfxac.mongodb.net/?retryWrites=true&w=majority";//env 파일에서 가져다쓰고싶으면 process.env.변수명
 new MongoClient(url).connect().then((client) => {
   console.log('DB연결성공')
   db = client.db('forum')
-  let 조건 = [
+    //여기부터
+    let 조건 = [
     { $match : {operationType : 'insert'}}
     ]
-    //change stream 사용법 감시하는거임
+            //change stream 사용법 감시하는거임
     changeStream = db.collection<PostType>('post').watch(/*조건일때만 아래 코드 실행해줌*/조건)
+    //여기까지는 changestream 부분
 
   app.listen(8080/*env 파일에서 가져다쓰고싶으면 process.env.변수명*/, () => {
     console.log('http://localhost:8080 에서 서버 실행중') //MongoClient 함수 안에 있는거야
@@ -38,9 +41,9 @@ new MongoClient(url).connect().then((client) => {
 app.get('/', async (요청: Request, 응답: Response) => {
     응답.sendFile(__dirname + '/index.html')
 })
+//여기까지는 복붙
 
-
-///////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 interface PostType {
     _id : ObjectId,
     title: string,
@@ -50,7 +53,7 @@ app.get('/list', async (요청:Request, 응답:Response)=>{
     let 디비에있는게시물 = await db.collection<PostType>('post').find().toArray()
     응답.render('list.ejs',{ 게시물 : 디비에있는게시물})
 })
-/////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 //SSE 사용방법
 app.get('/stream/list',(요청:Request, 응답:Response)=>{
     응답.writeHead(200, {
@@ -69,7 +72,7 @@ app.get('/stream/list',(요청:Request, 응답:Response)=>{
         응답.write(`data: ${JSON.stringify(변동사항.fullDocument)}\n\n`)
     })
 })
-/////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.get('/write', async (요청:Request, 응답:Response)=>{
     응답.render('write.ejs')
